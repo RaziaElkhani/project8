d7b1682071b299763ebe01010756b77860217a01
elkha <elkha@10.0.0.142>
2025-01-30 12:06:07 -0800

    creating a new array

---
+++ src/demo2/TestDemo2.java
@@ -0,0 +1,10 @@
+package demo2;
+
+public class TestDemo2 {
+public static void main(String[] args) {
+	int arr[]= {2,3,4,4,4,4,5,5,6,6,9,9,9};
+	for (int i =0 ;i<arr.length;i++) {
+		System.out.println(arr[i]);
+	}
+}
+}
